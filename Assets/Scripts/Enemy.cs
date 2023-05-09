30446d298f3adb3d04c66b4893a60d1c468a3540
Diomid <77359539+Diolmid@users.noreply.github.com>
2023-05-09 19:52:10 +0300

    Some Improvements

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,46 +9,70 @@ public class Enemy : LivingEntity
 
     [SerializeField] private float pathRefreshRate = 0.25f;
 
+    private float _damage = 1;
     private float _attackDistanceThreshold = 0.5f;
     private float _timeBetweenAttack = 1;
     private float _nextAttackTime;
     private float _myCollisionRadius;
     private float _targetCollisionRadius;
+    private bool _hasTarget;
+    private bool _hasPlayerOnScene;
 
     private NavMeshAgent _navMesh;
     private Transform _target;
+    private LivingEntity _targetEntity;
 
     private State _currentState;
 
     private void Awake()
     {
         _navMesh = GetComponent<NavMeshAgent>();
-        _target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        _hasPlayerOnScene = GameObject.FindGameObjectWithTag("Player") != null;
+        if (_hasPlayerOnScene)
+        {
+            _hasTarget = true;
+            _targetEntity = _target.GetComponent<LivingEntity>();
+            _target = GameObject.FindGameObjectWithTag("Player").transform;
+
+            _targetCollisionRadius = _target.GetComponent<CapsuleCollider>().radius;
+            _myCollisionRadius = GetComponent<CapsuleCollider>().radius;
+        }
     }
 
     protected override void Start()
     {
         base.Start();
 
-        _currentState = State.Chesing;
-
-        _targetCollisionRadius = _target.GetComponent<CapsuleCollider>().radius;
-        _myCollisionRadius = GetComponent<CapsuleCollider>().radius;
+        if (_hasPlayerOnScene)
+        {
+            _currentState = State.Chesing;  
+            _targetEntity.OnDeath += OnTargetDeath;
+        }
 
         StartCoroutine(UpdatePath());
     }
 
     private void Update()
     {
-        if(Time.time > _nextAttackTime)
+        if (_hasTarget)
         {
-            float sqrDisToTarget = (_target.position - transform.position).sqrMagnitude;
-            if (sqrDisToTarget < Mathf.Pow(_attackDistanceThreshold + _myCollisionRadius + _targetCollisionRadius, 2))
+            if(Time.time > _nextAttackTime)
             {
-                _nextAttackTime = Time.time + _timeBetweenAttack;
-                StartCoroutine(Attack());
+                float sqrDisToTarget = (_target.position - transform.position).sqrMagnitude;
+                if (sqrDisToTarget < Mathf.Pow(_attackDistanceThreshold + _myCollisionRadius + _targetCollisionRadius, 2))
+                {
+                    _nextAttackTime = Time.time + _timeBetweenAttack;
+                    StartCoroutine(Attack());
+                }
             }
         }
+    }
+
+    private void OnTargetDeath()
+    {
+        _hasTarget = false;
+        _currentState = State.Idle;
     }
 
     private IEnumerator Attack()
@@ -63,8 +87,15 @@ public class Enemy : LivingEntity
         float percent = 0;
         float attackSpeed = 3;
 
-        while (percent <= 1) 
+        bool hasAppliedDamage = false;
+
+        while (percent <= 1)
         {
+            if (percent >= 0.5f && !hasAppliedDamage)
+            {
+                hasAppliedDamage = true;
+                _targetEntity.TakeDamage(_damage);
+            }
             percent += Time.deltaTime * attackSpeed;
             float interpolation = (-Mathf.Pow(percent, 2) + percent) * 4;
             transform.position = Vector3.Lerp(originalPosition, attackPosition, interpolation);
@@ -78,7 +109,7 @@ public class Enemy : LivingEntity
 
     private IEnumerator UpdatePath()
     {
-        while(_target != null && !dead)
+        while(_hasTarget && !dead)
         {
             if(_currentState == State.Chesing)
             {
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,10 +1,23 @@
 using UnityEngine;
+using UnityEngine.InputSystem.HID;
 
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float damage = 1f;
     [SerializeField] private float speed = 10f;
     [SerializeField] protected LayerMask collisionMask;
+
+    private float _lifetime = 3;
+    private float _distanceModifier = 0.1f;
+
+    private void Start()
+    {
+        Destroy(gameObject, _lifetime);
+
+        var initialCollisions = Physics.OverlapSphere(transform.position, 0.1f, collisionMask);
+        if (initialCollisions.Length > 0 )
+            OnHitObject(initialCollisions[0]);
+    }
 
     void Update()
     {
@@ -24,7 +37,7 @@ public class Projectile : MonoBehaviour
         Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
 
-        if(Physics.Raycast(ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide))
+        if(Physics.Raycast(ray, out hit, moveDistance + _distanceModifier, collisionMask, QueryTriggerInteraction.Collide))
             OnHitObject(hit);
     }
 
@@ -33,6 +46,15 @@ public class Projectile : MonoBehaviour
         var damageableObject = hit.collider.GetComponent<IDamageable>();
         if(damageableObject != null )
             damageableObject.TakeHit(damage, hit);
+
+        Destroy(gameObject);
+    }
+
+    private void OnHitObject(Collider collider)
+    {
+        var damageableObject = collider.GetComponent<IDamageable>();
+        if (damageableObject != null)
+            damageableObject.TakeDamage(damage);
 
         Destroy(gameObject);
     }
